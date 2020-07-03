9393f3cf549185a3e6ec9b141048ca990bcc71ec
Marina <marinafaenze@hotmail.com>
2020-07-03 10:31:47 -0300

    jsx structure created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="app">
+      <h1 className="title">How fast do you type?</h1>
+      <textarea />
+      <h1 className="total-words">22</h1>
+      <button>START</button>
+      <h4 className="time-counter">Time reminaing: 15</h4>
     </div>
   );
 }
