e009287f161e689d2d6743fa2e633a15ae5f8fcd
Marina <marinafaenze@hotmail.com>
2020-07-03 17:18:51 -0300

    startGame endGame disabled elements

--- src/App.js
+++ src/App.js
@@ -2,37 +2,50 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  const [ text, setText ] = useState("");
-  const [ countTimer, setCountTimer ] = useState(0);
-  const [isTimeRunning, setIsTimeRunning] = useState(false)
-  const [ wordCount, setWorkCount ] = useState(0);
+  const INITIAL_TIME = 5;
 
-  const handleChange = (e) => setText(e.target.value);
+  const [ text, setText ] = useState("");
+  const [ countTimer, setCountTimer ] = useState(INITIAL_TIME);
+  const [ isTimeRunning, setIsTimeRunning ] = useState(false)
+  const [ wordCount, setWorkCount ] = useState(0);
 
   const calculateWords = (text) => {
     const totalWords = text.trim().split(" ").filter(word => word !== "").length;
     return totalWords;    
   };
 
+  const handleChange = (e) => setText(e.target.value);
+
+  const startGame = () => {
+    setIsTimeRunning(true);    
+    setCountTimer(INITIAL_TIME);
+    setText("");
+  };
+
+  const resetGame = () => {
+    setIsTimeRunning(false);
+    setWorkCount(calculateWords(text));
+  }
+
   useEffect(() => {
-    if(isTimeRunning && countTimer > 0){
-    setTimeout(() => {
+    (isTimeRunning && countTimer > 0)
+    ? setTimeout(() => {
         setCountTimer(prevCount => prevCount - 1);
       }, 1000)
-    } else if (countTimer === 0) {
-      setIsTimeRunning(false);
-      setWorkCount(calculateWords(text));
-    }
+    : (countTimer === 0) && resetGame();   
+
   }, [countTimer, isTimeRunning]);
 
   return (
     <div className="app">
       <h1 className="title">How fast do you type?</h1>
-
-      <textarea value={text} onChange={handleChange} />
-      
+      <textarea 
+        value={text} 
+        onChange={handleChange} 
+        disabled={!isTimeRunning}
+      />
       <h1 className="total-words">{wordCount}</h1>
-      <button onClick={() => setIsTimeRunning(true)}>START</button>
+      <button disabled={isTimeRunning} onClick={startGame}>START</button>
       <h4 className="count-timer">Time reminaing: {countTimer}</h4>
     </div>
   );
